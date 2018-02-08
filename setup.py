23bb9aa0bd1278bbd0c78931d679d3fddad8c7eb
pythonist2 <olafsk123@gmail.com>
2018-02-08 20:29:03 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='marketpy',
+      version='0.1',
+      description='Python library for financial modeling',
+      url='http://github.com/storborg/funniest',
+      author='Olaf Skrabacz',
+      author_email='olafsk123@gmail.com',
+      license='MIT',
+      packages=['marketpy'],
+      zip_safe=False)
